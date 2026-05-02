bca5cf34cd4c11dd2019d35ffcca4f8b3cb219d2
Schwamm2 <98617245+Schwamm2@users.noreply.github.com>
2026-05-02 02:26:37 +0200

    V1.3.2
    Version 1.3.2

--- bordcomputer/database.js
+++ bordcomputer/database.js
@@ -17,7 +17,7 @@ const busSystemData = {
    stops: [
     { id: 0.1, name: 'Schwamm Stadt, Hauptbahnhof', time: '0:00' },
     { id: 0.2, name: 'Schwamm Stadt, Busplatz', time: '0:00' },
-    { id: 1, name: 'Schwamm Stadt, Famesstrase', time: '0:00' },
+    { id: 1, name: 'Schwamm Stadt, Famesstrasse', time: '0:00' },
     { id: 2, name: 'Schwamm Stadt, Bogalastrasse', time: '0:00' },
     { id: 3, name: 'Schwamm Stadt, Kandis-Weg', time: '0:00' },
     { id: 4, name: 'Schwamm Stadt, Nord', time: '0:00' },
